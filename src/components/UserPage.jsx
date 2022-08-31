389fede9d8c71e8ece13c178fb18d38f7be85897
”Ismail” <ibeno95@mail.ru>
2022-08-31 12:07:23 +0300

    сделал стили на страницу авторизации и немного на основной странице

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -7,18 +7,18 @@ import { useDispatch } from "react-redux/es/hooks/useDispatch";
 import { fetchUsers, returnbook } from "../feauters/userSlice";
 
 const UserPage = () => {
-    const id = useSelector((state)=> state.application.id)
+  const id = useSelector((state) => state.application.id);
   const users = useSelector((state) => state.user.users);
- 
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  const handleClick = (idbook)=>{
-    dispatch(returnbook({id,idbook}))
-    dispatch(fetchUsers())
-  }
+  const handleClick = (idbook) => {
+    dispatch(returnbook({ id, idbook }));
+    dispatch(fetchUsers());
+  };
 
   return users.map((item) => {
     if (item._id === id) {
@@ -29,23 +29,30 @@ const UserPage = () => {
           </div>
           <div className={styles.user_text}>
             <h2>{item.login}</h2>
-            <div >{item.rent.map((i)=>{
-                return(
-                    <>
-                   <div className={styles.userBookCard}> 
-                <div className={styles.userBookImg}>
-                   <img src={`http://localhost:4000/images/${i.img}`} alt="" />
-                   <div> <h4>{i.name}</h4></div>
-                </div>
-                
-                <div className={styles.userBookBtn}>
-                    <button onClick={()=> handleClick(i._id)}>X</button>
-                </div>
-                </div> 
-               
-                </>
-                )
-            })}</div>
+            <div>
+              {item.rent.map((i) => {
+                return (
+                  <>
+                    <div className={styles.userBookCard}>
+                      <div className={styles.userBookImg}>
+                        <img
+                          src={`http://localhost:4000/images/${i.img}`}
+                          alt=""
+                        />
+                        <div>
+                          {" "}
+                          <h4>{i.name}</h4>
+                        </div>
+                      </div>
+
+                      <div className={styles.userBookBtn}>
+                        <button onClick={() => handleClick(i._id)}>X</button>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       );
