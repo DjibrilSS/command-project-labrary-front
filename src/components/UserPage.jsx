6e4c4a4d75e8074ba770c1fd7c166149bb64ec0f
”Ismail” <ibeno95@mail.ru>
2022-08-31 16:25:37 +0300

    сделал стили личного кабинета и главной страницы и сделал лого

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import axios from "axios"
-import avatar from "../components/dSxCxs3Vgzk.jpg";
+import axios from "axios";
 import styles from "../components/user.module.css";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useEffect } from "react";
@@ -10,15 +9,11 @@ import { useState } from "react";
 
 const UserPage = () => {
   const users = useSelector((state) => state.user.users);
-<<<<<<< HEAD
   const id = useSelector((state) => state.application.id);
-=======
-  const id = useSelector((state)=> state.application.id)
 
   const [img, setImg] = useState(null);
   const [file, setfile] = useState();
   const [avatar1, setAvatar1] = useState(null);
->>>>>>> main
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchUsers());
@@ -29,42 +24,58 @@ const UserPage = () => {
     dispatch(fetchUsers());
   };
 
-  
+  const sendFile = React.useCallback(
+    async (e) => {
+      e.preventDefault();
+      try {
+        const data = new FormData();
+        data.append("avatar", img);
 
-  const sendFile = React.useCallback(async (e) => {
-    
-    e.preventDefault()
-    try {
-      const data = new FormData();
-      data.append("avatar", img);
+        await axios
+          .post("/api/upload", data, {
+            headers: {
+              "content-type": "mulpipart/form-data",
+            },
+          })
 
-      await axios
-        .post("/api/upload", data, {
-          headers: {
-            "content-type": "mulpipart/form-data",
-          },
-        })
+          .then((res) => {
+            setAvatar1(res.data.path);
 
-        .then((res) => {
-          setAvatar1(res.data.path);
-
-          setfile(res.data.originalname);
-          dispatch(patchavatar({id,file}))
-        });
-       
-       
-    } catch (error) {}
-  }, [img]);
-
+            setfile(res.data.originalname);
+            dispatch(patchavatar({ id, file }));
+          });
+      } catch (error) {}
+    },
+    [img]
+  );
 
   return users.map((item) => {
     if (item._id === id) {
       return (
         <div className={styles.userpage}>
           <div className={styles.user_img}>
-<<<<<<< HEAD
             <h2>{item.login}</h2>
-            <img src={avatar} alt="" />
+            {avatar1 ? (
+              <img src={`${avatar1}`} alt="" />
+            ) : (
+              <img src={`http://localhost:4000/images/${item.avatar}`} alt="" />
+            )}
+            <input
+              className={styles.choose_file}
+              onChange={(e) => setImg(e.target.files[0])}
+              type="file"
+            />
+            <div>
+              <button className={styles.download_avatar} onClick={sendFile}>
+                Загрузить Аватар
+              </button>
+              <button
+                className={styles.download_avatar}
+                onClick={() => dispatch(patchavatar({ id, file }))}
+              >
+                Применить
+              </button>
+            </div>
           </div>
           <div className={styles.user_text}>
             <div>
@@ -77,6 +88,7 @@ const UserPage = () => {
                           src={`http://localhost:4000/images/${i.img}`}
                           alt=""
                         />
+
                         <div>
                           {" "}
                           <h4>{i.name}</h4>
@@ -97,38 +109,6 @@ const UserPage = () => {
                 );
               })}
             </div>
-=======
-            {avatar1 ? <img src={`${avatar1}`} alt="" /> 
-            :
-            <img src={`http://localhost:4000/images/${item.avatar}`} alt="" />}
-            <input onChange={(e)=> setImg(e.target.files[0])} type="file" />
-                   <button onClick={sendFile}>Загрузить  Аватар</button>
-                   <button onClick={()=>dispatch(patchavatar({id,file}))}>Применить</button>
-          </div>
-          <div className={styles.user_text}>
-            <h2>{item.login}</h2>
-            <div >{item.rent.map((i)=>{
-                return(
-                    <>
-                   <div className={styles.userBookCard}> 
-                <div className={styles.userBookImg}>
-                   <img src={`http://localhost:4000/images/${i.img}`} alt="" />
-              
-                   
-                   
-                   <div> <h4>{i.name}</h4></div>
-                </div>
-                
-                <div className={styles.userBookBtn}>
-                    <button onClick={()=> handleClick(i._id)}>X</button>
-                </div>
-                </div> 
-                <a href={`http://localhost:4000/pdf/${i.pdf}`}><button>читать</button></a>
-                <a href={`http://localhost:4000/fb2/${i.fb2}`}><button>скачать</button></a>
-                </>
-                )
-            })}</div>
->>>>>>> main
           </div>
         </div>
       );
